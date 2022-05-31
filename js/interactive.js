39e2fd5348fd8d09af9f8dafd7623004b53f4c73
cvivianc <viviuntiy@gmail.com>
2022-05-31 17:02:09 +1200

    enter key to submit form :zap:

--- js/interactive.js
+++ js/interactive.js
@@ -1,6 +1,17 @@
 const createbtn = document.getElementById("createbtn");
 
+
 createbtn.addEventListener('click', function() {
     var post = document.getElementById("input").value;
     alert(post);
+});
+
+var element = document.getElementById("input");
+
+element.addEventListener("keypress", function(event) {
+    if (event.key === "Enter") {
+        var postenter = document.getElementById("input").value;
+        alert(postenter);
+        event.preventDefault();
+    }
 });
