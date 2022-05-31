27b17a7e103a0b50832b9ff018b0a73c58256ba5
cvivianc <viviuntiy@gmail.com>
2022-05-31 23:26:15 +1200

    align logo and text in tab

--- js/interactive.js
+++ js/interactive.js
@@ -1,6 +1,3 @@
-const createbtn = document.getElementById("createbtn");
-
-
 createbtn.addEventListener('click', function() {
     var post = document.getElementById("input").value;
     alert(post);
