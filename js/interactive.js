f4a62fa12e98cc54d0bad9ca0bf3ed2dd9acfa5c
cvivianc <viviuntiy@gmail.com>
2022-06-02 23:04:56 +1200

    JS create text input, profile and reply btn

--- js/interactive.js
+++ js/interactive.js
@@ -1,22 +1,40 @@
-function createprofile() {
-    let profileimg = document.createElement('img');
-    let div = document.getElementsByClassName('discussion');
-    profileimg.className = 'profile';
-    profileimg.src = 'img/profile.svg';
-    div.appendChild(profileimg);
+function addreplybtn() {
+    let addreply = document.createElement('button');
+    const replybtn = document.querySelector('.threads')
+    addreply.className = 'replystyle';
+    replybtn.prepend(addreply);
 }
 
+function addprofileimg() {
+    let addprofile = document.createElement('img');
+    const profileimg = document.querySelector('.threads')
+    addprofile.className = 'profile';
+    addprofile.src = '../img/profile.svg'
+    profileimg.prepend(addprofile);
+}
 
 createbtn.addEventListener('click', function() {
+    // let input = document.getElementById("input");
+    // let create = document.getElementsByClassName('threads')[0];
+    // create.insertAdjacentHTML(
+    //     'afterbegin',
+    //     `<p>${input.value}</p>`
+    // );
+
+    // input.value = '';
+    // create.insertAdjacentHTML(
+    //     'afterbegin',
+    // );
+
     var input = document.getElementById("input");
-    let creatpost = document.getElementsByTagName('p')[0];
+    let creatpost = document.getElementsByClassName('threads')[0];
     let post = document.createElement('p');
 
     post.textContent = input.value;
-    creatpost.appendChild(post);
-    createprofile();
+    creatpost.prepend(post);
+    addreplybtn();
+    addprofileimg();
     input.value = '';
-
 
 });
 
@@ -30,6 +48,7 @@ element.addEventListener("keypress", function(event) {
         console.log(postenter.value);
         postenter.value = '';
     }
+
 });
 
 // btncreate.addEventListener('click', () => {
